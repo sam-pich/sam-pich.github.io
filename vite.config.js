c5121e5c4b1ebb8045f8946b4b9d7218d372b0e7
sam-pich <98869501+sam-pich@users.noreply.github.com>
2024-03-11 16:47:25 -0400

    v1.06

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sam-pich.github.io/'
+  base: '/sam-pich.github.io/', // Set the base path for GitHub Pages
+  build: {
+    outDir: 'dist', // Output directory for the build
+    emptyOutDir: true, // Clear the output directory before building
+    rollupOptions: {
+      input: {
+        main: '/index.html', // Use the root index.html as the entry point
+      },
+    },
+  },
 })
