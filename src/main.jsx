ed00ddbdc4c3224cf94daa156816de1775cdfba5
sam-pich <98869501+sam-pich@users.noreply.github.com>
2024-03-11 18:10:46 -0400

    xxxxxxx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App' // Update this line
+import App from './App' // Update this line
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sam-pich.github.io/', // Set the base path for GitHub Pages
+  base: '/sam-pich.github.io/',
   build: {
-    outDir: 'dist', // Output directory for the build
-    emptyOutDir: true, // Clear the output directory before building
+    outDir: 'dist',
+    emptyOutDir: true,
   },
-  root: './', // Add this line to specify the root directory
   resolve: {
     alias: {
-      '@': '/src' // Add this line to alias the 'src' directory
-    }
-  }
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
