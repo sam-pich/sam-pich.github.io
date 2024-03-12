eb1df8c8928e5829a32dd73faf5a2b1ac945d41c
sam-pich <98869501+sam-pich@users.noreply.github.com>
2024-03-11 21:36:00 -0400

    21

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -8,10 +7,5 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
   },
 })
