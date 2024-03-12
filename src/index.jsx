e006550e430d723439793846a3989eb94ba10105
sam-pich <98869501+sam-pich@users.noreply.github.com>
2024-03-12 02:32:55 -0400

    email

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,20 @@ import "./index.css";
 
 import winbg from "./assets/windowsbg.jpg";
 import me from "./assets/sssss.gif";
+import mail from "./assets/puter-mail.gif";
 
 function Index() {
   // change
 
   return (
 
-      <div id="root" style={{ backgroundImage: `url(${winbg})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', minHeight: '100vh' }}>
+      <div id="root" style={{
+          backgroundImage: `url(${winbg})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          minHeight: '100vh'
+      }}>
 
 
           <head>
@@ -30,7 +37,7 @@ function Index() {
 
 
           </>
-          <div className="window w-2/5 translate-x-10 translate-y-5">
+          <div className="z-0 fixed window w-[70vh] translate-x-[13vh] translate-y-[8vh]">
               <div className="title-bar">
                   <div className="title-bar-text">Intro</div>
                   <div className="title-bar-controls">
@@ -40,16 +47,31 @@ function Index() {
                   </div>
               </div>
               <div className="window-body font-bold text-3xl">
-                  <p>Hey! I'm Sam Pich</p>
+                  <p>Hey! I'm Sam Pich ------------------↓</p>
                   <p className="text-2xl">CompSci/Digital Humanities student at University of Ottawa</p>
                   <p className="text-xl">Skateboarder, videomaker, bookworm</p>
 
               </div>
-
-
           </div>
 
-          <div className="window w-1/5 translate-x-[55vh] translate-y-[-4vh]">
+          <div className="fixed window w-1/4 h-[21vh] translate-x-[20vh] translate-y-[27.5vh]">
+              <div className="title-bar">
+                  <div className="title-bar-text"></div>
+                  <div className="title-bar-controls">
+                      <button aria-label="Minimize"></button>
+                      <button aria-label="Maximize"></button>
+                      <button aria-label="Close"></button>
+                  </div>
+              </div>
+              <div className="window-body font-bold text-3xl relative w-[45vh]">
+                  <p className="text-xl">Aspiring UX designer/product designer, I'll make anything and everything given
+                      the tools
+                      <br/><br/>Or at least <i>try</i> to</p>
+
+              </div>
+          </div>
+
+          <div className="fixed z-2 window w-1/5 translate-x-[61.5vh] translate-y-[21vh]">
               <div className="title-bar">
                   <div className="title-bar-text">me.gif</div>
                   <div className="title-bar-controls">
@@ -61,12 +83,27 @@ function Index() {
               <div>
                   <img src={me} alt="me"/>
               </div>
+          </div>
 
-
+          <div className="fixed window w-[50vh] h-[25vh] translate-x-[8vh] translate-y-[55vh]">
+              <div className="title-bar">
+                  <div className="title-bar-text">Email</div>
+                  <div className="title-bar-controls">
+                      <button aria-label="Minimize"></button>
+                      <button aria-label="Maximize"></button>
+                      <button aria-label="Close"></button>
+                  </div>
+              </div>
+              <div className="window-body font-bold text-3xl spacing">
+                  <p className="text-xl">Say hi/Meet for coffee/Collab on ideas</p>
+                    <button className="z-0 w-3/4 h-[7vh] translate-x-[5.5vh] translate-y-[3.5vh]" href="mailto:sam.pich@outlook.com">Email me!</button>
+              </div>
+              <div>
+                    <img className="z-1 w-[10vh] absolute translate-x-[8.5vh] translate-y-[-2.5vh]" src={mail} alt="mail"/>
+              </div>
           </div>
 
       </div>
-
 
 
   );
