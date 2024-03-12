bb3c41d7addf1f71edb14d2a5bd249eb3f168b72
sam-pich <98869501+sam-pich@users.noreply.github.com>
2024-03-12 00:10:31 -0400

    workflow

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sam-pich.github.io/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
 })
