2637e6288f4a34ba118098629fd3dd3a64f8485b
sam-pich <98869501+sam-pich@users.noreply.github.com>
2024-03-12 16:45:23 -0400

    resumetabs

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,30 @@
 import "98.css";
 import "./index.css";
-
+import React, {useState} from "react";
 import winbg from "./assets/windowsbg.jpg";
 import me from "./assets/sssss.gif";
 import mail from "./assets/puter-mail.gif";
+import samresume from "./assets/Sam_Pich_Resume.pdf";
 
 function Index() {
-  // change
+    const [selectedTab, setSelectedTab] = useState('list');
+
+    const handleTabClick = (event) => {
+        const selectedTabElement = event.currentTarget;
+        const tabId = selectedTabElement.getAttribute('data-tab');
+
+        // Remove 'aria-selected' from all tabs
+        const tabs = selectedTabElement.parentElement.querySelectorAll('[role="tab"]');
+        tabs.forEach((tab) => {
+            tab.setAttribute('aria-selected', 'false');
+        });
+
+        // Set 'aria-selected' to 'true' for the clicked tab
+        selectedTabElement.setAttribute('aria-selected', 'true');
+
+        // Update the selectedTab state variable
+        setSelectedTab(tabId);
+    };
 
   return (
 
@@ -17,6 +35,7 @@ function Index() {
           backgroundRepeat: 'no-repeat',
           minHeight: '100vh'
       }}>
+
 
 
           <head>
@@ -35,7 +54,50 @@ function Index() {
 
               </div>
 
-
+              <div className="absolute window-body w-auto left-[97.5vh] top-[5.5vh]">
+                  <menu role="tablist">
+                      <li role="tab" aria-selected={selectedTab === 'list'} onClick={handleTabClick} data-tab="list">
+                          <a href="#tabs" className={"font-bold"}>List</a>
+                      </li>
+                      <li role="tab" aria-selected={selectedTab === 'resume'} onClick={handleTabClick}
+                          data-tab="resume">
+                          <a href="#tabs" className={"font-bold"}>Resume</a>
+                      </li>
+                      <li role="tab" aria-selected={selectedTab === 'video'} onClick={handleTabClick} data-tab="video">
+                          <a href="#tabs" className={"font-bold"}>Video Showcase</a>
+                      </li>
+                      <li role="tab" aria-selected={selectedTab === 'about'} onClick={handleTabClick} data-tab="about">
+                          <a href="#tabs" className={"font-bold"}>About this site</a>
+                      </li>
+                  </menu>
+                  <div className="window w-auto h-auto" role="tabpanel">
+                      {selectedTab === 'list' && (
+                          <div key={selectedTab} id="list" className="window-body">
+                              <p>Desktop tab content</p>
+                          </div>
+                      )}
+                      {selectedTab === 'resume' && (
+                          <div key={selectedTab} id="resume" className="relative window-body">
+                              <iframe
+                                  key={selectedTab}
+                                  className={"w-[70vh] h-[80vh]"}
+                                  src={samresume}
+                                  title="Resume"
+                              />
+                          </div>
+                      )}
+                      {selectedTab === 'video' && (
+                          <div key={selectedTab} id="video" className="window-body">
+                              <p>My computer tab content</p>
+                          </div>
+                      )}
+                      {selectedTab === 'about' && (
+                          <div key={selectedTab} id="about" className="window-body">
+                              <p>Control panel tab content</p>
+                          </div>
+                      )}
+                  </div>
+              </div>
           </>
           <div className="z-0 relative window w-2/5 translate-x-[13vh] translate-y-[8vh]">
               <div className="title-bar">
@@ -83,7 +145,6 @@ function Index() {
                       <img src={me} alt="me"/>
                   </div>
               </div>
-
           </div>
 
 
@@ -105,17 +166,16 @@ function Index() {
                               <img className="z-1  absolute w-[10vb] translate-x-[-11.5vb]" src={mail}
                                    alt="mail"/>
                               Email me!</a>
-
                       </button>
-                      <div>
 
-                      </div>
                   </div>
               </div>
-
           </div>
 
+
+
       </div>
+
 
 
   );
