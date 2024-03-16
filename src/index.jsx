06dde5f63de37b55e75034e4ff9c0809220464df
sam-pich <98869501+sam-pich@users.noreply.github.com>
2024-03-16 19:43:00 -0400

    download button

--- src/index.jsx
+++ src/index.jsx
@@ -119,10 +119,24 @@ function Index() {
                           <div key={selectedTab} id="resume" className="relative window-body">
                               <iframe
                                   key={selectedTab}
-                                  className={"w-[70vh] h-[80vh]"}
+                                  className="w-[70vh] h-[80vh]"
                                   src={samresume}
                                   title="Resume"
                               />
+                              <a
+                                  href={samresume}
+                                  download="Sam_Pich_Resume.pdf"
+                                  className="absolute bottom-4 right-4 bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+                              >
+                                  <svg
+                                      className="fill-current w-4 h-4 mr-2"
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      viewBox="0 0 20 20"
+                                  >
+                                      <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+                                  </svg>
+                                  <span>Download</span>
+                              </a>
                           </div>
                       )}
                       {selectedTab === 'video' && (
@@ -151,7 +165,7 @@ function Index() {
                                   part?</p>
                               <p className={"text-2xl font-bold italic"}>The stuff reads good.</p>
                               <p className={"text-2xl "}>Good. Not great. <br/>
-                                  Gets the job done. Lasting impression longer than a minute?
+                                  Gets the job done. With a lasting impression?
                                   <br/><i>Not really.</i></p>
                               <p className={"text-2xl font-bold"}>Different isn't always better.</p>
                               <p className={"text-2xl "}>
